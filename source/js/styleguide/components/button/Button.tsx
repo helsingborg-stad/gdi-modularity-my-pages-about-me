51994a000028ce2ec94a7d168a7f475243caace1
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-11-25 09:06:54 +0100

    add button props & modifiers

--- source/js/styleguide/components/button/Button.tsx
+++ source/js/styleguide/components/button/Button.tsx
@@ -2,14 +2,46 @@ import React from 'react'
 
 
 interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+	color?: 'default' | 'primary' | 'secondary'
+	variant?: 'filled' | 'basic' | 'outline'
+	size?: 'sm' | 'md' | 'lg'
 	disabled?: boolean
 }
 
-const Button = ({ className, children, onClick, ...props }: Props ): JSX.Element => (
+const propsToClassNamesMap = {
+	size: {
+		sm: 'c-button--sm',
+		md: 'c-button--md',
+		lg: 'c-button--lg',
+	},
+	variant: {
+		filled: 'c-button__filled',
+		basic: 'c-button__basic',
+		outline: 'c-button__outline',
+	},
+	color: {
+		primary: '--primary',
+		secondary: '--secondary',
+		default: '--default',
+	},
+	disabled: 'is-disabled',
+}
+
+const Button = ({ 
+	className, 
+	children, 
+	onClick, 
+	color = 'default', 
+	size = 'md', 
+	variant = 'filled', 
+	...props 
+}: Props ): JSX.Element => (
 	<button
 		className={[
 			'c-button',
-			'c-button--md',
+			propsToClassNamesMap.size[size],
+			propsToClassNamesMap.variant[variant],
+			propsToClassNamesMap.variant[variant]+propsToClassNamesMap.color[color],
 			...className ? [className] : [],
 		].join(' ')}
 		onClick={(e) => {
