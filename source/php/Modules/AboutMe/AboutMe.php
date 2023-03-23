97f7a73e02cbcb0073aef85c46ba1e24d2e66bdd
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-03-23 17:05:51 +0100

    fix: get custom form labels

--- source/php/Modules/AboutMe/AboutMe.php
+++ source/php/Modules/AboutMe/AboutMe.php
@@ -22,35 +22,35 @@ class AboutMe extends \Modularity\Module
 		return [
 			'aboutMeApiUri' => get_field('about_me_api_uri', 'options'),
 			'labels' => [
-				'email_label' 							=> get_field('email_label', $this->ID)
-					?? __('Email', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'phone_label' 							=> get_field('phone_label', $this->ID)
-					?? __('Phone', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'email_placeholder' 				=> get_field('email_placeholder', $this->ID)
-					?? __('Email', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'phone_placeholder' 				=> get_field('phone_placeholder', $this->ID)
-					?? __('Phone number', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'button_cancel' 						=> get_field('button_cancel', $this->ID)
-					?? __('Cancel', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'button_save' 							=> get_field('button_save', $this->ID)
-					?? __('Save', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'button_edit' 							=> get_field('button_edit', $this->ID)
-					?? __('Edit', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'phone_is_unverified' 			=> get_field('phone_is_unverified', $this->ID)
-					?? __('Your phone number is not verified.', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'email_is_unverified' 			=> get_field('email_is_unverified', $this->ID)
-					?? __('Your email is not verified.', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'application_error' 				=> get_field('application_error', $this->ID)
-					?? __('Something went wrong, please try again later', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'form_terms' 								=> get_field('form_terms', $this->ID) ?? '',
-				'resend_verification_sms' 	=> get_field('resend_verification_sms', $this->ID)
-					?? __('Resend SMS', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'sent_verification_sms' 		=> get_field('sent_verification_sms', $this->ID)
-					?? __('Sent SMS to {phone}', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'resend_verification_email' 	=> get_field('resend_verification_email', $this->ID)
-					?? __('Resend mail', GDI_ABOUT_ME_TEXT_DOMAIN),
-				'sent_verification_email' 	=> get_field('sent_verification_email', $this->ID)
-					?? __('Sent mail to {email}', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'email_label' 							=> get_field('labels_email_label', $this->ID)
+					?: __('Email', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'phone_label' 							=> get_field('labels_phone_label', $this->ID)
+					?: __('Phone', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'email_placeholder' 				=> get_field('labels_email_placeholder', $this->ID)
+					?: __('Email', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'phone_placeholder' 				=> get_field('labels_phone_placeholder', $this->ID)
+					?: __('Phone number', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'button_cancel' 						=> get_field('labels_button_cancel', $this->ID)
+					?: __('Cancel', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'button_save' 							=> get_field('labels_button_save', $this->ID)
+					?: __('Save', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'button_edit' 							=> get_field('labels_button_edit', $this->ID)
+					?: __('Edit', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'phone_is_unverified' 			=> get_field('labels_phone_is_unverified', $this->ID)
+					?: __('Your phone number is not verified.', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'email_is_unverified' 			=> get_field('labels_email_is_unverified', $this->ID)
+					?: __('Your email is not verified.', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'application_error' 				=> get_field('labels_application_error', $this->ID)
+					?: __('Something went wrong, please try again later', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'form_terms' 								=> get_field('form_terms', $this->ID) ?: '',
+				'resend_verification_sms' 	=> get_field('labels_resend_verification_sms', $this->ID)
+					?: __('Resend SMS', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'sent_verification_sms' 		=> get_field('labels_sent_verification_sms', $this->ID)
+					?: __('Sent SMS to {phone}', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'resend_verification_email' 	=> get_field('labels_resend_verification_email', $this->ID)
+					?: __('Resend mail', GDI_ABOUT_ME_TEXT_DOMAIN),
+				'sent_verification_email' 	=> get_field('labels_sent_verification_email', $this->ID)
+					?: __('Sent mail to {email}', GDI_ABOUT_ME_TEXT_DOMAIN),
 			],
 		];
 	}
